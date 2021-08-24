6122ce3e00f11421a05f921b8eff17e223ad3574
zhaill <zhaill@tico.cn>
2021-08-24 11:45:19 +0800

    add:自定义指令

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
     <input v-fbind:data="n" />
     <!-- <Transition /> -->
     <PraticeEventBus />
+    <MyDirective />
   </div>
 </template>
 
 <script>
 // import Transition from "./components/transitionC";
 import PraticeEventBus from "./components/praticeEventBus";
+import MyDirective from "./components/myDirectives";
 export default {
   name: "App",
   components: {
     // Transition,
     PraticeEventBus,
+    MyDirective,
   },
   data() {
     return {
---
+++ src/components/myDirectives/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <ul>
+    <li
+      v-for="item in list"
+      :key="item.id"
+      v-clickedDirective:propKey="item.text"
+    >
+      {{ item.text }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        {
+          id: 1,
+          text: "第一个",
+        },
+        {
+          id: 2,
+          text: "第二个",
+        },
+        {
+          id: 3,
+          text: "第三个",
+        },
+      ],
+    };
+  },
+  directives: {
+    clickedDirective: {
+      //当绑定的元素插入到dom时
+      inserted(el, data) {
+        //data {arg: propKey, value: item.text  }
+        el.addEventListener("click", () => {
+          console.log(data.value);
+        });
+      },
+      bind() {},
+      update() {},
+      componentUpdated() {},
+      unbind() {},
+    },
+  },
+};
+</script>
+
+<style>
+</style>
